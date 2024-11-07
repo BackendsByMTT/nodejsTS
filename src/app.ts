fb951f5ac5c6f938afa502345ed668836b83ed9d
MohitNegi007 <mohitnegi45801@gmail.com>
2024-11-07 22:30:48 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(`${process.env.value} World`);
+});
+
+app.listen(port, () => {
+  return console.log(`Expresss is listening at http://localhost:${port}`);
+});
